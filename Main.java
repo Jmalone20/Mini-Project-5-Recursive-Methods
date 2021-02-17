0376f628d208f9ab83e3292f7a8790ac21221e05
Jmalone20 <jasmine.malone@patelhs.org>
2021-02-17 17:20:06 +0000

    added an else if statement as well as another else statement

--- Main.java
+++ Main.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-   Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
 
-   System.out.println(" What is your name? ");
-   String name = scan.next();
+    System.out.println(" What is your name? ");
+    String name = scan.next();
 
-   attendance(name,0);
+    attendance(name, 0);
   }
-  public static void attendance(String name, int num ){
-   String [] names = {"Sam","Jade", "Jasmine", "Lucy","Cole" };
-   if( name == name[i])
-   return true;
-   }else{ 
 
-   }
+  public static boolean attendance(String name, int i) {
+    String[] names = { "Sam", "Jade", "Jasmine", "Lucy", "Cole" };
+    if (name == names[i]) {
+      return true;
+    } else if (i < 10) {
+      return attendance(name, i);
+    }else{
+      return false;
+    }
+  }
 }
