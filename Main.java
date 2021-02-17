c99bf229a124ed4e657b3f8e0271e52460da371c
Jmalone20 <jasmine.malone@patelhs.org>
2021-02-17 16:56:58 +0000

    added a print statement in my main method

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-   
-  }
-  public static void attendance(){
+   Scanner scan = new Scanner(System.in);
 
+   System.out.println(" What is your name? ");
+   String name = scan.next();
+
+   attendance(name,0);
   }
+  public static void attendance(String name, int num ){
+   String [] names = {"Sam","Jade", "Jasmine", "Lucy","Cole" };
+   if( name == name[i])
+   return true;
+   }else{
+
+   }
 }
